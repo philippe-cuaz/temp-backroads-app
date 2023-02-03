2662ebf74ffea0f5bce77a2e5c88728a5ae9f18b
Philippe Cuaz <philippe.cuaz@orange.fr>
2023-02-03 12:20:53 +0100

    Update Footer.js
    second commit correcting typos

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
 
       <ul className="footer-icons">
         {socialLinks.map((link) => {
-          const { id, href, icon } = link;
+          
           return <SocialLink key={link.id} {...link} itemClass="footer-icon" />;
         })}
       </ul>
